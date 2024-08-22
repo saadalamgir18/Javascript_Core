c5c318f3ef7e302da898e79a65fb51fb18b16a59
saadalamgir18 <saadalamgir18@gmail.com>
2024-08-23 02:24:16 +0500

    add the winning feature

--- Pig-Game/README.md
+++ Pig-Game/README.md
@@ -1,1 +1,13 @@
 # Pig Game
+
+This is a practice project for manipulating DOM with JS. The features of this game are
+
+1. roll the dice and genrate number from 1 to 6. if 1 comes switch the player
+2. calculte current number on dice roll w.r.t actice player
+3. add the score to the actice player on clicking hold button
+4. winn the game if score is >= 100
+5. disable the buttons and hide the dice on winning
+6. reset the game on clicking NEW GAME button
+
+![Alt text](pig-game-flowchart.png?raw=true "Title")
+![Alt text](game.png?raw=true "Title")
--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -13,29 +13,58 @@ const current1El = document.getElementById("current--1");
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
 // starting condition
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+function switchPlayer() {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+}
 // rolling dice funcationality
 btnRoll.addEventListener("click", function () {
-  // genrating random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  //   diplay the dice
-  diceEl.classList.remove("hidden");
-  diceEl.src = `dice-${dice}.png`;
-  //   check for roll == 1 if true switch to next player
-  if (dice !== 1) {
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-    // add dice to the current score
-  } else {
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    currentScore = 0;
-    player0El.classList.toggle("player--active");
-    player1El.classList.toggle("player--active");
+  if (playing) {
+    // genrating random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    //   diplay the dice
+    diceEl.classList.remove("hidden");
+    diceEl.src = `dice-${dice}.png`;
+    //   check for roll == 1 if true switch to next player
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+      // add dice to the current score
+    } else {
+      switchPlayer();
+    }
+  }
+});
+btnHold.addEventListener("click", function () {
+  if (playing) {
+    // add current score to active player
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    // check if players score is >= 100
+    if (scores[activePlayer] >= 15) {
+      //   finsih the game
+      playing = false;
+      diceEl.classList.add("hidden");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove("player--active");
+    } else {
+      //switch to next player
+      switchPlayer();
+    }
   }
 });
