0829f0cf9d4cbec7459a151518e80426d2c0deac
saadalamgir18 <saadalamgir18@gmail.com>
2024-08-22 16:52:38 +0500

    solved challange 8 for arrays and array methods with for loop

---
+++ challange8/coding_challange#8.js
@@ -0,0 +1,24 @@
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+function calcTip(bill) {
+  if (bill >= 50 && bill <= 300) {
+    return bill * 0.15;
+  } else {
+    return bill * 0.2;
+  }
+}
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(bills[i] + tip);
+}
+
+function calcAverage(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+}
+console.log(calcAverage(totals));
