d94730482d8475da35ddd4a4f0bd61943c3f66a7
saadalamgir18 <saadalamgir18@gmail.com>
2024-08-27 17:47:02 +0500

    initiated bankist app project

--- Functions/script.js
+++ Functions/script.js
@@ -1,1 +1,76 @@
 "use strict";
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  return booking;
+};
+
+// Functions Accepting Callback Functions
+const oneWrod = function (str) {
+  return str.replace(/ /g, "").toLowerCase();
+};
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
+};
+
+// higher order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer("JavaScript is the best!", upperFirstWord);
+
+// Functions Returning Functions
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet("hey");
+greeterHey("saad");
+greeterHey("muneeb");
+greet("hello")("saad");
+
+const greet1 = (greeting) => (name) => console.log(`${greeting} ${name}`);
+
+greet1("hi")("saad");
+const lufthansa = {
+  airline: "lufthansa",
+  iataCode: "LH",
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+lufthansa.book(239, "saad");
+lufthansa.book(635, "muneeb");
+
+(function () {
+  console.log("hello");
+})();
+
+const secureBooking = function () {
+  let passengerCount = 0;
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+const booker = secureBooking();
+booker();
