0feb09a1cdf1c336e9e1a05acd89f5a946e81e82
saadalamgir18 <saadalamgir18@gmail.com>
2024-09-10 13:46:35 +0500

    Implementing Smooth Scrolling

--- Advanced-DOM-Bankist/script.js
+++ Advanced-DOM-Bankist/script.js
@@ -34,16 +34,17 @@ document.addEventListener("keydown", function (e) {
 ////////////////////////////////////////
 
 // Selecting Eleent
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
 const header = document.querySelector(".header");
-console.log(document.querySelectorAll(".section"));
-console.log(document.getElementById("section--1"));
+// console.log(document.querySelectorAll(".section"));
+// console.log(document.getElementById("section--1"));
 const allButtons = document.getElementsByTagName("button");
-console.log(allButtons);
-console.log(document.getElementsByClassName("btn"));
+// console.log(allButtons);
+// console.log(document.getElementsByClassName("btn"));
 
+/*
 // Creating and Inserting elements
 // .insertAdjacentHTML
 const message = document.createElement("div");
@@ -59,3 +60,41 @@ document
   .addEventListener("click", function () {
     message.remove();
   });
+
+// styles
+message.style.backgroundColor = "#37383d";
+message.style.width = "120%";
+console.log(message.style.backgroundColor);
+console.log(getComputedStyle(message).color);
+message.style.height =
+  parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
+
+document.documentElement.style.setProperty("--color-primary", "orangered");
+
+// Atribute
+
+const logo = document.querySelector(".nav__logo");
+console.log(logo.alt);
+console.log(logo.className);
+logo.alt = "Beautifull Minimilist Logo";
+logo.setAttribute("Comapny", "Bankist");
+console.log(logo.src);
+console.log(logo.getAttribute("src"));
+
+*/
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.getElementById("section--1");
+btnScrollTo.addEventListener("click", function (e) {
+  const s1Cords = section1.getBoundingClientRect();
+  console.log(e.target.getBoundingClientRect());
+  // window.scrollTo(
+  //   s1Cords.left + window.pageXOffset,
+  //   s1Cords.top + window.pageYOffset
+  // );
+  // window.scrollTo({
+  //   left: s1Cords.left + window.pageXOffset,
+  //   top: s1Cords.top + window.pageYOffset,
+  //   behavior: "smooth",
+  // });
+  section1.scrollIntoView({ behavior: "smooth" });
+});
