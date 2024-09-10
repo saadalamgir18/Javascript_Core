0b9f8326c8c49b69c2e88717423ba35425c9aa17
saadalamgir18 <saadalamgir18@gmail.com>
2024-09-11 00:07:11 +0500

    Implementing sticky navigation

--- Advanced-DOM-Bankist/script.js
+++ Advanced-DOM-Bankist/script.js
@@ -7,7 +7,10 @@ const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.getElementById("section--1");
 const header = document.querySelector(".header");
 const allButtons = document.getElementsByTagName("button");
-
+const nav = document.querySelector(".nav");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
 ///////////////////////////////////////
 // Modal window
 
@@ -54,6 +57,9 @@ btnScrollTo.addEventListener("click", function (e) {
 ////////////////////////////////////////
 
 // Page Navigation
+
+////////////////////////////////////////
+
 // document.querySelectorAll(".nav__link").forEach((el) => {
 //   el.addEventListener("click", function (e) {
 //     e.preventDefault();
@@ -76,6 +82,86 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
+////////////////////////////////////////
+
+// Tabed componenet
+
+////////////////////////////////////////
+
+// tabs.forEach((t) =>
+//   t.addEventListener("click", function () {
+//     console.log("TAB");
+//   })
+// );
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+
+  if (!clicked) return;
+  // Remove actice tab
+  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+
+  tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
+  //activate tab
+  clicked.classList.add("operations__tab--active");
+
+  //activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
+
+////////////////////////////////////////
+
+// Manu Fade animation
+
+////////////////////////////////////////
+
+const handleHover = function (e) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+nav.addEventListener("mouseout", handleHover.bind(1));
+////////////////////////////////////////
+
+//Stricky Navigation
+
+////////////////////////////////////////
+
+// const initialCords = section1.getBoundingClientRect();
+// window.addEventListener("scroll", function () {
+//   console.log(window.scrollY);
+//   if (window.scrollY > initialCords.top) {
+//     nav.classList.add("sticky");
+//   } else {
+//     nav.classList.remove("sticky");
+//   }
+// });
+
+//Stricky Navigation: Intersection Obbserver API
+const navHeight = nav.getBoundingClientRect().height;
+const observerCallback = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+};
+const observerOpt = {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+};
+const observer = new IntersectionObserver(observerCallback, observerOpt);
+observer.observe(header);
 ////////////////////////////////////////
 
 // Selecting Eleent
@@ -160,5 +246,34 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
 });
 document.querySelector(".nav").addEventListener("click", function (e) {
   this.style.backgroundColor = randomColor();
+});
+
+
+// traversing DOM
+
+const h1 = document.querySelector("h1");
+
+// going downwards: child
+console.log(h1.querySelectorAll(".highlight"));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = "white";
+h1.lastElementChild.style.color = "orangered";
+
+// going upwards: parants
+
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+h1.closest(".header").style.background = "var(--gradient-secondary)";
+h1.closest("h1").style.background = "var(--gradient-primary)";
+
+// going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) {
+    el.style.transform = "scale(0.5)";
+  }
 });
 */
